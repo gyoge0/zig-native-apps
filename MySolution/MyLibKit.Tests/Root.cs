e40051c65f9cb4d1a29afb94edd36c837f94abc3
Yogesh Thambidurai <yogesh@gyoge.com>
2025-01-08 17:52:14 -0500

    feat: add test project

---
+++ MySolution/MyLibKit.Tests/Root.cs
@@ -0,0 +1,12 @@
+﻿namespace MyLibKit.Tests;
+
+using static MylibKit.Root;
+
+public class Root
+{
+    [Fact]
+    public void BasicAddFunctionality()
+    {
+        Assert.Equal(10, add(3, 7));
+    }
+}
