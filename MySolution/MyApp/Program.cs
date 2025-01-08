f6a9953a00e0fc89f37ed6f38d79542ee73fe3b8
Yogesh Thambidurai <yogesh@gyoge.com>
2025-01-08 17:47:45 -0500

    chore: initial commit

---
+++ MySolution/MyApp/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using static MylibKit.Root;
+
+int result = add(1, 2);
+
+Console.WriteLine($"1 + 2 = {result}");
---
+++ MySolution/MylibKit/Root.cs
@@ -0,0 +1,9 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MylibKit;
+
+public static class Root
+{
+    [DllImport("mylib")]
+    public static extern int add(int a, int b);
+}
