0eabfe55ac70df62c8fe01c4112437e7ae577208
Yogesh Thambidurai <yogesh@gyoge.com>
2025-01-09 08:37:45 -0500

    fix(build.zig): don't use root mod so zig 0.13 works

--- build.zig
+++ build.zig
@@ -18,22 +18,27 @@ pub fn build(b: *std.Build) void {
     // This creates a "module", which represents a collection of source files alongside
     // some compilation options, such as optimization mode and linked system libraries.
     // Every executable or library we compile will be based on one or more modules.
-    const lib_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = b.path("src/root.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    // Use this in zig 0.14
+    // const lib_mod = b.createModule(.{
+    //     // `root_source_file` is the Zig "entry point" of the module. If a module
+    //     // only contains e.g. external object files, you can make this `null`.
+    //     // In this case the main source file is merely a path, however, in more
+    //     // complicated build scripts, this could be a generated file.
+    //     .root_source_file = b.path("src/root.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
     // Now, we will create a static library based on the module we created above.
     // This creates a `std.Build.Step.Compile`, which is the build step responsible
     // for actually invoking the compiler.
     const lib = b.addStaticLibrary(.{
         .name = "mylib",
-        .root_module = lib_mod,
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+        // use this in Zig 0.14
+        // .root_module = lib_mod,
     });
 
     // This declares intent for the library to be installed into the standard
@@ -43,7 +48,11 @@ pub fn build(b: *std.Build) void {
 
     const shared_lib = b.addSharedLibrary(.{
         .name = "mylib",
-        .root_module = lib_mod,
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+        // use this in Zig 0.14
+        // .root_module = lib_mod,
     });
 
     b.installArtifact(shared_lib);
@@ -51,7 +60,11 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
-        .root_module = lib_mod,
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+        // use this in Zig 0.14
+        // .root_module = lib_mod,
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
